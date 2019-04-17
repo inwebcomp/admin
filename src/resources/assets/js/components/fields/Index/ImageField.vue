79c80c94ff5f8da0f4c86eabe55fffbf06b3ff9d
Alexander Topalo <saneock19@gmail.com>
2019-04-17 22:40:50 +0300

    Fix image field width on index

--- src/resources/assets/js/components/fields/Index/ImageField.vue
+++ src/resources/assets/js/components/fields/Index/ImageField.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <img v-if="field.value" class="m-auto block" :src="img(field.value)" />
-        <div style="width: 42px" v-if="! field.valu"></div>
+        <div style="width: 42px" v-if="! field.value"></div>
     </div>
 </template>
 
