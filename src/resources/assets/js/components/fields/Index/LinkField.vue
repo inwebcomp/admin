4f8a32bd3a15974bc0e228f742b1279598891bc5
Alexander Topalo <inwebcomp@gmail.com>
2019-07-01 12:38:36 +0300

    Change base framework

--- src/resources/assets/js/components/fields/Index/LinkField.vue
+++ src/resources/assets/js/components/fields/Index/LinkField.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-link :to="field.link" class="data-table__value__link whitespace-no-wrap">{{ field.value }}</router-link>
+    <router-link :to="field.link" class="data-table__value__link">{{ field.value }}</router-link>
 </template>
 
 <script>
