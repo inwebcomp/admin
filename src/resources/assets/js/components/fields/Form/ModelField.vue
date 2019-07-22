7ff43e342259bb4730ccead37d3babf6f82e3951
Escral <inwebcomp@gmail.com>
2019-07-22 19:55:53 +0300

    Update order

--- src/resources/assets/js/components/fields/Form/ModelField.vue
+++ src/resources/assets/js/components/fields/Form/ModelField.vue
@@ -7,17 +7,21 @@
                         @input="$emit('input', $event)"
                         v-bind="extraAttributes"
                         @search="search"
-                        :class="errorClasses()">
+                        :class="errorClasses()"
+                        ref="select">
                 <template v-slot="{ options }">
                     <ul class="dropdown__values search__values">
-                        <li v-for="(option, $i) in options" :key="$i" class="dropdown__option" @click="select(option.value)">
+                        <li v-for="(option, $i) in options" :key="$i" class="dropdown__option"
+                            @click="select(option.value)">
                         <span v-if="option.image" class="dropdown__option__image"
                               :style="{ 'background-image': 'url(' + option.image + ')' }"></span>
 
                             <div class="dropdown__option__text search__option__text">
                                 <div class="search__option__title">{{ option.title }}</div>
                                 <div class="search__option__subtitle">{{ option.subTitle }}</div>
-                                <div class="search__option__id">ID: {{ option.resourceId }}<i v-if="! option.visibility" class="fas fa-eye-slash  text-grey ml-2"></i></div>
+                                <div class="search__option__id">ID: {{ option.resourceId }}<i v-if="! option.visibility"
+                                                                                              class="fas fa-eye-slash  text-grey ml-2"></i>
+                                </div>
                             </div>
                         </li>
                     </ul>
@@ -50,9 +54,9 @@
         },
 
         data() {
-          return {
-              options: []
-          }
+            return {
+                options: []
+            }
         },
 
         methods: {
@@ -68,7 +72,8 @@
             },
 
             select(value) {
-                this.handleChange(value)
+                // this.handleChange(value)
+                this.$refs.select.select(value)
             }
         },
     }
