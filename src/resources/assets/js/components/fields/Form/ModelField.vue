8a05841d3bcfec3daa87d9979d23060795467473
Escral <inwebcomp@gmail.com>
2019-07-22 19:43:55 +0300

    Update order

--- src/resources/assets/js/components/fields/Form/ModelField.vue
+++ src/resources/assets/js/components/fields/Form/ModelField.vue
@@ -8,6 +8,20 @@
                         v-bind="extraAttributes"
                         @search="search"
                         :class="errorClasses()">
+                <template v-slot="{ options }">
+                    <ul class="dropdown__values search__values">
+                        <li v-for="(option, $i) in options" :key="$i" class="dropdown__option" @click="select(option.value)">
+                        <span v-if="option.image" class="dropdown__option__image"
+                              :style="{ 'background-image': 'url(' + option.image + ')' }"></span>
+
+                            <div class="dropdown__option__text search__option__text">
+                                <div class="search__option__title">{{ option.title }}</div>
+                                <div class="search__option__subtitle">{{ option.subTitle }}</div>
+                                <div class="search__option__id">ID: {{ option.resourceId }}<i v-if="! option.visibility" class="fas fa-eye-slash  text-grey ml-2"></i></div>
+                            </div>
+                        </li>
+                    </ul>
+                </template>
             </app-select>
         </template>
     </default-field>
