060ff21781fa106db10939d1e8f1dd9ab1778ac4
Escral <inwebcomp@gmail.com>
2019-09-23 09:54:32 +0300

    Remove Authorize middleware

--- src/App/AdminRoute.php
+++ src/App/AdminRoute.php
@@ -29,7 +29,6 @@ class AdminRoute
                 StartSession::class,
                 AddQueuedCookiesToResponse::class,
                 DispatchServingAdminEvent::class,
-                Authorize::class,
             ])
             ->as('admin.api::')
             ->prefix(Admin::path() . '/api')
