b496b2c8d92d8282370615204ee2cf19bbcc1d3b
Escral <inwebcomp@gmail.com>
2022-07-24 23:45:27 +0300

    tag: 9.0.3

--- database/migrations/2022_17_05_100000_action_events_add_description_column.php
+++ database/migrations/2022_17_05_100000_action_events_add_description_column.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AdminUsersAddNameColumn extends Migration
+class ActionEventsAddDescriptionColumn extends Migration
 {
     /**
      * Run the migrations.
